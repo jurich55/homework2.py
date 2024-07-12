0f8bc27d3b4cd16d4eb5c325f47b6bd14b7794c8
Vladimir <sv_urich@mail.ru>
2024-07-12 11:37:15 +0400

    Add files via upload

---
+++ homework2.py/main.py
@@ -0,0 +1,6 @@
+number_of_tasks = 12
+quantity_of_hours = 1.5
+name = 'Python,'
+average_time = quantity_of_hours/number_of_tasks
+print('Курс:',name,'всего задач:',number_of_tasks,','' затрачено часов:',quantity_of_hours,','
+      ' среднее время выполнения:',average_time,'часа')
